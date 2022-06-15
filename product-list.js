a4048ca5d98c3a1f91f4b0be68263a82e3eb70c6
sanajitjana <dev.sanajitjana@gmail.com>
2022-06-15 20:27:13 +0530

    top-right filterfunction is done

--- product-list.js
+++ product-list.js
@@ -9,13 +9,13 @@ let productList = [
     sub_name: "Get To You Printed Maxi Dress. Color Black Combo",
     price: "94.99",
     mrp: "118.00",
-    rating: "2",
+    rating: "5",
     favorite: "102",
     gender: "Women",
     type: "Clothing",
     brand: "Angel Maternity",
     color: "Black",
-    tag: "New",
+    tag: "Best Seller",
   },
   {
     img_url:
@@ -23,8 +23,7 @@ let productList = [
     name: "LAUREN Ralph Lauren",
     sub_name: "Foiled Jersey Cocktail Dress",
     price: "121.99",
-    mrp: "175.00",
-    rating: "0",
+    rating: "4",
     favorite: "14",
     gender: "Women",
     type: "Clothing",
@@ -39,7 +38,7 @@ let productList = [
     sub_name: "Truepace Woven Trainingsuit Jacket HG2007",
     price: "290.00",
     mrp: "290.00",
-    rating: "0",
+    rating: "3",
     favorite: "1",
     gender: "Women",
     type: "Clothing",
@@ -54,7 +53,7 @@ let productList = [
     sub_name: "Carrie Pullover",
     price: "118.00",
     mrp: "118.00",
-    rating: "0",
+    rating: "5",
     favorite: "0",
     gender: "Women",
     type: "Clothing",
@@ -69,7 +68,6 @@ let productList = [
     name: "Under Armour",
     sub_name: "Carrie Pullover",
     price: "17.50",
-    mrp: "25.00",
     rating: "4",
     favorite: "767",
     gender: "Men",
@@ -85,7 +83,7 @@ let productList = [
     sub_name: "Trefoil Tee",
     price: "13.84",
     mrp: "30.00",
-    rating: "5",
+    rating: "2",
     favorite: "74",
     gender: "Men",
     type: "Clothing",
@@ -114,14 +112,13 @@ let productList = [
     name: "Bulwark FR",
     sub_name: "Midweight FR Dress Uniform Shirt, CAT 2",
     price: "84.00",
-    mrp: "84.00",
     rating: "5",
     favorite: "79",
     gender: "Men",
     type: "Clothing",
     brand: "Bulwark FR",
     color: "Navy",
-    tag: "New",
+    tag: "Best Seller",
   },
   {
     img_url:
@@ -130,7 +127,7 @@ let productList = [
     sub_name: "L1212 Classic Pique Polo Shirt",
     price: "57.99",
     mrp: "98.00",
-    rating: "4",
+    rating: "0",
     favorite: "270",
     gender: "Men",
     type: "Clothing",
@@ -177,7 +174,7 @@ let productList = [
     sub_name: "Classic Flip-Flop",
     price: "29.9",
     mrp: "35.00",
-    rating: "0",
+    rating: "4",
     favorite: "5",
     gender: "Women",
     type: "Shoes",
@@ -188,40 +185,127 @@ let productList = [
 ];
 
 let table = document.querySelector(".product-table");
-productList.forEach(function (ele) {
-  let link = document.createElement("a");
-  link.setAttribute("href", "product-details.html");
-  let card = document.createElement("div");
-  card.setAttribute("class", "cards");
-  let tag = document.createElement("div");
-  tag.setAttribute("class", "tag");
-  let fav = document.createElement("div");
-  fav.setAttribute("class", "fav");
-  if (ele.tag == "Style Room") {
-    tag.setAttribute("class", "style-room");
-  } else if (ele.tag == "Low Stock") {
-    tag.setAttribute("class", "low-stock");
-  }
-  tag.innerText = ele.tag;
-  let main_img = document.createElement("img");
-  main_img.setAttribute("class", "main-img");
-  main_img.setAttribute("src", ele.img_url);
-  main_img.setAttribute("alt", ele.name);
-  let text_content = document.createElement("div");
-  text_content.setAttribute("class", "text-content");
-  let name = document.createElement("h4");
-  name.innerText = ele.name;
-  let sub_name = document.createElement("h5");
-  sub_name.setAttribute("class", "sub-name");
-  sub_name.innerText = ele.sub_name;
-  let price = document.createElement("p");
-  price.setAttribute("class", "price");
-  price.innerText = ele.price;
-  let rating = document.createElement("div");
-  rating.setAttribute("class", "rating");
 
-  text_content.append(name, sub_name, price, rating);
-  card.append(tag, main_img, text_content);
-  link.append(card);
-  table.append(link);
-});
+displayData(productList);
+function displayData(data) {
+  table.innerHTML = "";
+  data.forEach(function (ele) {
+    //a tag
+    let link = document.createElement("a");
+    link.setAttribute("href", "product-details.html");
+
+    //card
+    let card = document.createElement("div");
+    card.setAttribute("class", "cards");
+
+    //tag
+    let tag = document.createElement("div");
+    if (ele.tag) {
+      tag.setAttribute("class", "tag");
+      tag.innerText = ele.tag;
+    }
+    if (ele.tag == "Style Room") {
+      tag.setAttribute("class", "style-room");
+    } else if (ele.tag == "Low Stock") {
+      tag.setAttribute("class", "low-stock");
+    }
+
+    //fav
+    let fav = document.createElement("div");
+    fav.setAttribute("class", "fav");
+
+    //image
+    let main_img = document.createElement("img");
+    main_img.setAttribute("class", "main-img");
+    main_img.setAttribute("src", ele.img_url);
+    main_img.setAttribute("alt", ele.name);
+
+    //below all text
+    let text_content = document.createElement("div");
+    text_content.setAttribute("class", "text-content");
+
+    //name
+    let name = document.createElement("h4");
+    name.innerText = ele.name;
+
+    //sub name
+    let sub_name = document.createElement("h5");
+    sub_name.setAttribute("class", "sub-name");
+    sub_name.innerText = ele.sub_name;
+
+    //price-mrp
+    let price_mrp = document.createElement("div");
+    price_mrp.setAttribute("class", "price-mrp-line");
+    let price = document.createElement("p");
+    let mrp = document.createElement("p");
+    if (ele.mrp) {
+      price.setAttribute("class", "price-mrp");
+      mrp.setAttribute("class", "mrp");
+      price_mrp.append(price, mrp);
+    } else {
+      price.setAttribute("class", "price");
+      price_mrp.append(price);
+    }
+    price.innerText = `$${ele.price}`;
+    mrp.innerText = `MSRP: $${ele.mrp}`;
+
+    //rating
+    let rating = document.createElement("div");
+    if (ele.rating == "5") {
+      rating.setAttribute("class", "rating-5");
+    } else if (ele.rating == "4") {
+      rating.setAttribute("class", "rating-4");
+    } else if (ele.rating == "3") {
+      rating.setAttribute("class", "rating-3");
+    } else if (ele.rating == "2") {
+      rating.setAttribute("class", "rating-2");
+    } else if (ele.rating == "1") {
+      rating.setAttribute("class", "rating-1");
+    }
+
+    //append all
+    text_content.append(name, sub_name, price_mrp, rating);
+    card.append(tag, main_img, text_content);
+    link.append(card);
+    table.append(link);
+  });
+}
+
+//sort-by-filter -> right-side-top
+let selectBox = document.querySelector("#top-right-filter");
+selectBox.addEventListener("change", filterFunction);
+function filterFunction() {
+  let newArray = [...productList];
+  let filteredArray = newArray.filter(function (item) {
+    if (selectBox.value == "All") {
+      return item;
+    } else if (selectBox.value == "Customer Rating") {
+      return sortByRating(newArray);
+    } else if (selectBox.value == "Price:-Low-to-High") {
+      return sortByPriceLowToHight(newArray);
+    } else if (selectBox.value == "Price:-High-to-Low") {
+      return sortByPriceHighToLow(newArray);
+    } else if (selectBox.value == item.tag) {
+      return item.tag;
+    }
+  });
+  displayData(filteredArray);
+}
+function sortByRating(newArray) {
+  let sortArray = newArray.sort(function (a, b) {
+    return b.rating - a.rating;
+  });
+  return sortArray;
+}
+function sortByPriceLowToHight(newArray) {
+  let lowToHigh = newArray.sort(function (a, b) {
+    return +a.price - +b.price;
+  });
+  return lowToHigh;
+}
+function sortByPriceHighToLow(newArray) {
+  let highToLow = newArray.sort(function (a, b) {
+    return +b.price - +a.price;
+  });
+  return highToLow;
+}
