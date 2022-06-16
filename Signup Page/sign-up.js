b7d90f299a722bd94cb66996d379f78ec6053182
sanajitjana <dev.sanajitjana@gmail.com>
2022-06-16 13:08:19 +0530

    sign-up page done

---
+++ Signup Page/sign-up.js
@@ -0,0 +1,39 @@
+let form = document.querySelector("form");
+let loginArrayData = JSON.parse(localStorage.getItem("loginData")) || [];
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  //   form validation
+  if (form.name.value == "") {
+    alert("Please enter a name");
+  } else if (form.email.value == "") {
+    alert("Please enter a email");
+  } else if (form.password.value == "") {
+    alert("Please enter a password");
+  } else if (form.re_password.value == "") {
+    alert("Please enter re_password");
+  } else if (form.password.value != form.re_password.value) {
+    alert("Incorrect Re-enter password");
+  } else {
+    //data store in object
+    let dataObj = {
+      name: form.name.value,
+      email: form.email.value,
+      password: form.password.value,
+      re_password: form.re_password.value,
+    };
+    loginArrayData.push(dataObj);
+    localStorage.setItem("loginData", JSON.stringify(loginArrayData));
+
+    //clear form after submit
+    form.name.value = "";
+    form.email.value = "";
+    form.password.value = "";
+    form.re_password.value = "";
+    alert("Welcome! Signup successful");
+
+    // store loginAuth to LocalStorage
+    localStorage.setItem("currentLogin", "true");
+  }
+});
